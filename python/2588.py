b4b11cc73da353fbd5273236fb1d509c6f1290af
HGH <rhkdgus1730@gmail.com>
2025-03-16 17:40:31 +0900

    시작

---
+++ python/2588.py
@@ -0,0 +1,10 @@
+first_number = input()
+second_number = input()
+
+fn = int(first_number)
+sn = int(second_number)
+
+print(fn*int(second_number[2]))
+print(fn*int(second_number[1]))
+print(fn*int(second_number[0]))
+print(fn*sn)
